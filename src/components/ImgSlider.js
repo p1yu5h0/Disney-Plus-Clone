791150f8487fc60aae5ada1f64d6b656bf0676be
p1yu5h0 <piyush200205@gmail.com>
2021-10-29 15:23:01 +0530

    new

---
+++ src/components/ImgSlider.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import styled from 'styled-components';
+import Slider from 'react-slick'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+function ImgSlider() {
+
+    var settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    };
+    return (
+        <Carousel {...settings}>
+            <Wrap>
+                <img src="/images/slider-badging.jpg"></img>
+            </Wrap>
+            <Wrap>
+                <img src="/images/slider-badag.jpg"></img>
+            </Wrap>
+            <Wrap>
+                <img src="/images/slider-scale.jpg"></img>
+            </Wrap>
+        </Carousel>
+    )
+}
+
+export default ImgSlider
+
+const Carousel = styled(Slider)`
+    margin-top: 40px;
+
+    ul li button {
+        &:before {
+          font-size: 10px;
+          color: rgb(150, 158, 171);
+        }
+    }
+
+    li.slick-active button:before {
+        color: white;
+    }
+
+    .slick-list {
+        overflow: visible;
+    }
+
+    button {
+        z-index: 1;
+    }
+`
+
+const Wrap = styled.div`
+    img{
+         border-radius: 4px;
+         width: 100%;
+         height: 100%;
+         box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
+         rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+         border: 4px solid transparent;
+    }
+`
