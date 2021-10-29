9225bcd6a7bd71e1037ee5549ae84339c1e2fee2
p1yu5h0 <piyush200205@gmail.com>
2021-10-29 12:21:22 +0530

    new files

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,21 +4,48 @@ import styled from 'styled-components'
 function Header() {
     return (
         <Nav>
-            <Logo src="" />
-            <NavMenu></NavMenu>
+            <Logo>
+                <img src="/images/logo.svg" alt="Disney+" />
+            </Logo>
+            <NavMenu>
+                <a> 
+                    <img src="/images/home-icon.svg"></img>
+                    <span>HOME</span>
+                </a>
+            </NavMenu>
         </Nav>
     )
 }
 
 export default Header
 
-const Nav = styled.div`
-    height: 70px;
-    background: #090b13
-`
-const Logo = styled.img`
+const Nav = styled.nav`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 70px;
+  background-color: #090b13;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 36px;
+  letter-spacing: 16px;
+  z-index: 3;
+`;
 
-`
+const Logo = styled.a`
+  padding: 0;
+  width: 80px;
+  margin-top: 4px;
+  max-height: 70px;
+  font-size: 0;
+  display: inline-block;
+  img {
+    display: block;
+    width: 100%;
+  }
+`;
 
 const NavMenu = styled.div`
 
